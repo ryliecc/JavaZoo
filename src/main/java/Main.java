ab0585a07f71e2cdb371849aa5e3ba089f85ffac
Rylie Castell <rylie.castell@gmail.com>
2023-09-04 10:35:55 +0200

    check automatic generated methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Animal elephant = new Animal(1, "Fanti", "elephant", 3);
+        Animal anotherElephant = new Animal(1, "Fanti", "elephant", 3);
+        System.out.println(elephant);
+        System.out.println(elephant.equals(anotherElephant));
 
     }
 }
