ed635dc737cb334da21d8e47d8b9a610eca1a85f
Rylie Castell <rylie.castell@gmail.com>
2023-09-04 11:05:59 +0200

    add more examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         Owner elephantOwner = new Owner("Michael Meier", 27, "Am Waldweg 3, 09113 Chemnitz");
+        Owner fishOwner = new Owner("Sandra Schmidt", 39, "Hubertushöhe 6, 01836 Dingsstadt");
+
         Species elephant = new Species("elephant", 150000);
+        Species dolphin = new Species("dolphin", 8000);
+        Species koi = new Species("koi", 20);
+
         Animal fanti = new Animal(1, "Fanti", elephant, 3, elephantOwner);
         Animal anotherFanti = new Animal(1, "Fanti", elephant, 3, elephantOwner);
+
+        Animal flipper = new Animal(2, "Flipper", dolphin, 4, fishOwner);
+        Animal twinkle = new Animal(3, "Twinkle", koi, 2, fishOwner);
+
         System.out.println(elephant);
-        System.out.println(fanti.equals(anotherFanti));
+        System.out.println("Fanti equals another Fanti: " + fanti.equals(anotherFanti));
 
     }
 }
